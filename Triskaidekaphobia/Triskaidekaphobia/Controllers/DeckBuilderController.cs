ccfd6539cb8a273fc7e5380f544f743e26e4118b
Unknown <jipyana@student.neumont.edu>
2017-12-07 16:17:22 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Triskaidekaphobia/Triskaidekaphobia/Controllers/DeckBuilderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Triskaidekaphobia.Controllers
+{
+    public class DeckBuilderController : Controller
+    {
+        // GET: DeckBuilder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
